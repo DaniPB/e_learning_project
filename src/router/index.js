0dbfa76b38aafc5b0785c53a01054a80252ebc7d
Daniela PB <danielapatinob98@gmail.com>
2021-11-24 23:02:19 -0500

    Adds: routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../components/Home.vue'
 import Login from '../components/Login.vue'
+import SignUp from '../components/SignUp.vue'
+import CoursesDashboard from '../components/CoursesDashboard.vue'
 
 const routes = [
   {
@@ -12,6 +14,16 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login
+  },
+  {
+    path: '/sign-up',
+    name: 'SignUp',
+    component: SignUp
+  },
+  {
+    path: '/courses-dashboard',
+    name: 'CoursesDashboard',
+    component: CoursesDashboard
   }
 ]
 
